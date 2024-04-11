bcfe96b6b5cda771208978af5710b72cacb50690
vitalyvitmens <otm_@tut.by>
2024-04-11 17:14:48 +0300

    chore: добавляет фото проектов

--- src/data/project.ts
+++ src/data/project.ts
@@ -5,35 +5,36 @@ import SiteTouchImg from './../assets/images/projects/site_touch.jpg'
 import SocialImg from './../assets/images/projects/social.jpg'
 import ToDoImg from './../assets/images/projects/todo.jpg'
 import PortfolioImg from './../assets/images/projects/portfolio.jpg'
-import GameLauncherImg from './../assets/images/projects/game_launcher.jpg'
-import DontPageImg from './../assets/images/projects/dont_page.jpg'
-import AppImg from './../assets/images/projects/app.jpg'
+import TodoImg from './../assets/images/projects/todo.jpg'
+import SocialPostImg from './../assets/images/projects/social-post.jpg'
+import NewsBlogImg from './../assets/images/projects/news-blog.jpg'
 
 export const ProjectData = [
   {
     title: 'News Blog',
-    image: AppImg,
+    image: NewsBlogImg,
     description:
-      'React Native; NestJS; MongoDB; Persist; Subscriptions; Ads and more...',
+      'Full Stack, React JS, Styled Components, Redux, MongoDB, Express, Mongoose',
     link: 'http://45.89.190.180/',
   },
   {
     title: 'Social Post',
-    image: DontPageImg,
-    description: 'Design, React, Animation, Invite System',
+    image: SocialPostImg,
+    description:
+      'Full Stack, React JS, Styled Components, Redux, MongoDB, Express, Mongoose',
     link: 'http://92.63.177.60/',
   },
   {
-    title: 'React Todo PWA',
-    image: GameLauncherImg,
-    description:
-      'Freelance, React Native, TypeScript, Java, Redux, and more...',
+    title: 'Todo',
+    image: TodoImg,
+    description: 'TypeScript, Vite, PWA, Service Workers, Dexie',
     link: 'https://vitalyvitmens.github.io/rs-react-todo/',
   },
   {
     title: 'Contact Book',
     image: ImageCounterImg,
-    description: 'React, hook, useState, LocalStorage, multi language clean css. To display pictures, turn on VPN',
+    description:
+      'React, hook, useState, LocalStorage, multi language clean css. To display pictures, turn on VPN',
     link: 'https://vitalyvitmens.github.io/rs-redux-toolkit-rtk-query-contacts/',
   },
   {
