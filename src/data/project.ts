7d8d052ee1f0085e1867623eea0dd4b8a55c1242
vitalyvitmens <otm_@tut.by>
2024-04-12 14:05:53 +0300

    feat(src/data/project.ts): создает project data

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,10 +1,8 @@
-import ImageCounterImg from './../assets/images/projects/counter.jpg'
-import LotteryImg from './../assets/images/projects/lottery.jpg'
-import LauncherImg from './../assets/images/projects/launcher.jpg'
-import SiteTouchImg from './../assets/images/projects/site_touch.jpg'
-import SocialImg from './../assets/images/projects/social.jpg'
-import ToDoImg from './../assets/images/projects/todo.jpg'
-import PortfolioImg from './../assets/images/projects/portfolio.jpg'
+import ContactBookImg from './../assets/images/projects/contact-book.jpg'
+import RickAndMortyImg from './../assets/images/projects/rick-and-morty.jpg'
+import FormsImg from './../assets/images/projects/forms.jpg'
+import CryptoInitialImg from './../assets/images/projects/crypto-initial.jpg'
+import MyPortfolioImg from './../assets/images/projects/my-portfolio.jpg'
 import TodoImg from './../assets/images/projects/todo.jpg'
 import SocialPostImg from './../assets/images/projects/social-post.jpg'
 import NewsBlogImg from './../assets/images/projects/news-blog.jpg'
@@ -15,54 +13,64 @@ export const ProjectData = [
     image: NewsBlogImg,
     description:
       'Full Stack, React JS, Styled Components, Redux, MongoDB, Express, Mongoose',
-    link: 'http://45.89.190.180/',
+    linkWeb: 'http://45.89.190.180/',
+    linkCode: 'https://github.com/vitalyvitmens/news-blog',
   },
   {
     title: 'Social Post',
     image: SocialPostImg,
     description:
       'Full Stack, React JS, Styled Components, Redux, MongoDB, Express, Mongoose',
-    link: 'http://92.63.177.60/',
+    linkWeb: 'http://92.63.177.60/',
+    linkCode: 'https://github.com/vitalyvitmens/socialpost',
   },
   {
     title: 'Todo',
     image: TodoImg,
     description: 'TypeScript, Vite, PWA, Service Workers, Dexie',
-    link: 'https://vitalyvitmens.github.io/rs-react-todo/',
+    linkWeb: 'https://vitalyvitmens.github.io/rs-react-todo/',
+    linkCode: 'https://github.com/vitalyvitmens/rs-react-todo',
   },
   {
     title: 'Contact Book',
-    image: ImageCounterImg,
+    image: ContactBookImg,
     description:
       'React, hook, useState, LocalStorage, multi language clean css. To display pictures, turn on VPN',
-    link: 'https://vitalyvitmens.github.io/rs-redux-toolkit-rtk-query-contacts/',
+    linkWeb:
+      'https://vitalyvitmens.github.io/rs-redux-toolkit-rtk-query-contacts/',
+    linkCode:
+      'https://github.com/vitalyvitmens/rs-redux-toolkit-rtk-query-contacts',
   },
   {
     title: 'Rick and Morty',
-    image: SiteTouchImg,
+    image: RickAndMortyImg,
     description:
       'React, hook, useState, LocalStorage, multi language clean css, and more.',
-    link: 'https://rickandmorty-c6ac8.web.app/',
-  },
-  {
-    title: 'My Portfolio',
-    image: PortfolioImg,
-    description:
-      'React, hook, useState, LocalStorage, multi language clean css, and more.',
-    link: '',
-  },
-  {
-    title: 'Crypto Initial',
-    image: ToDoImg,
-    description:
-      'React, Redux, API requests, clean css, axios, thunks, and more.',
-    link: 'https://vitalyvitmens.github.io/react-crypto-initial/',
+    linkWeb: 'https://rickandmorty-c6ac8.web.app/',
+    linkCode: 'https://github.com/vitalyvitmens/rs-react-optimization-PWA',
   },
   {
     title: 'Forms',
-    image: SocialImg,
+    image: FormsImg,
     description:
       'React, Redux, API requests, clean css, axios, thunks, and more.',
-    link: 'https://vitalyvitmens.github.io/rs-forms/',
+    linkWeb: 'https://vitalyvitmens.github.io/rs-forms/',
+    linkCode: 'https://github.com/vitalyvitmens/rs-forms',
+  },
+  {
+    title: 'Crypto Initial',
+    image: CryptoInitialImg,
+    description:
+      'React, Redux, API requests, clean css, axios, thunks, and more.',
+    linkWeb: 'https://vitalyvitmens.github.io/react-crypto-initial/',
+    linkCode: 'https://github.com/vitalyvitmens/react-crypto-initial',
+  },
+  {
+    title: 'My Portfolio',
+    image: MyPortfolioImg,
+    description:
+      'React, hook, useState, LocalStorage, multi language clean css, and more.',
+    linkWeb: '',
+    linkCode: 'https://github.com/vitalyvitmens/my-portfolio',
   },
 ]
