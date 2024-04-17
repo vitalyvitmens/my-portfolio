a72ca0864ac4206bafc31bebe9c5cf2d76be66a3
vitalyvitmens <otm_@tut.by>
2024-04-17 16:36:56 +0300

    refactor: рефакторит отображение погоды в зависимсоти от выбранного языка

--- src/components/WeatherDateInfo/WeatherDateInfo.tsx
+++ src/components/WeatherDateInfo/WeatherDateInfo.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { WEATHER_API_URL } from '../../constants/urls'
+import { WEATHER_API_KEY } from '../../constants/apiKeys'
+import { weatherApiBaseUrl } from '../../constants/baseUrls'
 import './WeatherDateInfo.scss'
 
 type FooterProps = {
@@ -8,19 +9,19 @@ type FooterProps = {
 }
 
 export const WeatherDateInfo: React.FC<FooterProps> = ({ language }) => {
-  const [city, setCity] = useState<string>('')
+  const [city, setCity] = useState<string>('Minsk')
   const [temperature, setTemperature] = useState<number | null>(null)
   const [weather, setWeather] = useState<string>('')
 
   useEffect(() => {
-    fetch(WEATHER_API_URL)
+    fetch(weatherApiBaseUrl(language) + WEATHER_API_KEY)
       .then((res) => res.json())
       .then(({ name, main, weather }) => {
         setCity(name)
         setTemperature(Math.round(main.temp))
         setWeather(weather[0].description)
       })
-  }, [])
+  }, [language])
 
   return (
     <div className="weather">
--- src/constants/baseUrls.ts
+++ src/constants/baseUrls.ts
@@ -1,3 +1,3 @@
 export const FORMSPREE_BASE_URL = 'https://formspree.io/f/'
-export const WEATHER_API_BASE_URL =
-  'https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&lang=ru&appid='
+export const weatherApiBaseUrl = (language: string) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&lang=${language}&appid=`
