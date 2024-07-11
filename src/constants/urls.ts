08f78bf7ddc0b616bccde28808e6ed2c88ea32b5
vitalyvitmens <otm_@tut.by>
2024-07-11 15:35:33 +0300

    chore: add CV-ru & CV-en

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,6 +3,6 @@ import { FORMSPREE_API_KEY } from './apiKeys'
 
 export const FORMSPREE_URL = FORMSPREE_BASE_URL + FORMSPREE_API_KEY
 export const CV_RU_URL =
-  'https://raw.githubusercontent.com/vitalyvitmens/my-portfolio/172b728558c3f52ae63495f99d1ac15ad42475c8/src/assets/files/CV-ru.pdf'
+  'https://raw.githubusercontent.com/vitalyvitmens/my-portfolio/f9f5e24d2e21d05e3966dbb17239912993a61b8b/src/assets/files/CV-ru.pdf'
 export const CV_EN_URL =
-  'https://raw.githubusercontent.com/vitalyvitmens/my-portfolio/172b728558c3f52ae63495f99d1ac15ad42475c8/src/assets/files/CV-en.pdf'
+  'https://raw.githubusercontent.com/vitalyvitmens/my-portfolio/f9f5e24d2e21d05e3966dbb17239912993a61b8b/src/assets/files/CV-en.pdf'
